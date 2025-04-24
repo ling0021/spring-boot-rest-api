71630b3b37e1117fe2c7d79c2bdd9c5811e22bc2
Ling <yl461019633@gmail.com>
2025-04-25 06:56:05 +0800

    Completed the development for get api

--- src/main/java/in/ling/restapi/repository/ExpenseRepository.java
+++ src/main/java/in/ling/restapi/repository/ExpenseRepository.java
@@ -3,5 +3,19 @@ package in.ling.restapi.repository;
 import in.ling.restapi.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+/**
+ * Repository interface for managing ExpenseEntity objects.
+ * This interface extends JpaRepository to provide CRUD operations.
+ * @author Ling
+ */
+
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+    /**
+     * Find an expense by its ID.
+     * @param expenseId the ID of the expense
+     * @return an Optional containing the found ExpenseEntity, or empty if not found
+     */
+    Optional<ExpenseEntity> findByExpenseId(String expenseId);
 }
